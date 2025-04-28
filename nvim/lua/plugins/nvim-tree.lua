a9e6677072495f574633058f1d90939ebfc4c4a5
0x2261 <0x2261@pm.me>
2025-04-28 10:29:12 -0400

    first two configs that were on-hand on a tty server

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,41 @@
+-- adds a file explorer split
+return {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("nvim-tree").setup {
+            sort = {
+                folders_first = true,
+                files_first = false,
+            },
+            view = {
+                side = "right",
+                width = 35,
+                --number = true,
+                --relativenumber = true,
+                float = {
+                    enable = true,
+                    quit_on_focus_loss = true,
+                    open_win_config = {
+                        relative = "editor",
+                        border = "rounded",
+                        width = 75,
+                        height = 48,
+                        col = 999,
+                        row = 0
+                    },
+                },
+            },
+            renderer = {
+                indent_markers = {
+                    enable = true,
+                    inline_arrows = true,
+                }
+            }
+        }
+    end,
+}
